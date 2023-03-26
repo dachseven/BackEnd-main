9f7e3159fd2a9108227f4f690d5b6850ddf1658d
dachseven <oviedoagustin46337@gmail.com>
2023-03-26 19:44:46 -0300

    arreglitosv2

--- src/main/java/com/portfolio/dach/Security/MainSecurity.java
+++ src/main/java/com/portfolio/dach/Security/MainSecurity.java
@@ -70,7 +70,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
                 cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));                
   cc.setAllowedOrigins(Arrays.asList("/*"));
   cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH", "DELETE"));
-                cc.addAllowedOrigin("https://https://frontenddach.web.app");
+                cc.addAllowedOrigin("https://frontenddach.web.app");
                 cc.setMaxAge(Duration.ZERO);
                 cc.setAllowCredentials(Boolean.TRUE);
   UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
